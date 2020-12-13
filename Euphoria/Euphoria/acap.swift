bec807e56a7655083181f55bd622314465ed5e99
Daria Bloodworth <daria@yay.ee>
2020-12-13 06:19:59 -0700

    Adding gitignore

---
+++ Euphoria/Euphoria/acap.swift
@@ -0,0 +1,15 @@
+/*
+    acap.swift
+    Euphoria
+    
+    Created by Daria Bloodworth on 2020-12-13.
+
+    Copyright (c) 2020 by Yay Digital, OÜ
+ 
+    Simplified ACAP handling
+
+    Original commentary:
+    There are lots of things not up to snuff here.  If we're really going to use ACAP as intended by ACAP Nazis, it will have to be made much less efficient.
+*/
+
+import Foundation
---
+++ Euphoria/Euphoria/address.swift
@@ -0,0 +1,12 @@
+/*
+    address.swift
+    Euphoria
+
+    Created by Daria Bloodworth on 2020-12-13.
+
+    Copyright (c) 1990-1992 by the University of Illinois Board of Trustees
+    Copyright (c) 1993-1996 by Qualcomm Incorporated
+    Copyright (c) 2020 by Yay Digital, OÜ
+*/
+
+import Foundation
